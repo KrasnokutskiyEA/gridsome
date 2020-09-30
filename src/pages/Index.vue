080c4ddc24b276c8cb56880b9400815fe2b71ab5
krasnokutskiy.e <Krasnokutskiy.E@ostec-group.ru>
2020-10-01 02:09:20 +0300

    WIP

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,7 +5,7 @@ export default {
   }
 }
 </script>
-<!--
+
 <page-query>
 query {
   allGoogleSheet {
@@ -19,12 +19,12 @@ query {
   }
 }
 </page-query>
--->
+
 <template>
   <Layout>
 
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-
+    <!--
     <g-image alt="Example image" src="~/favicon.png" width="135" />
 
     <h1>Hello, world!</h1>
@@ -37,12 +37,11 @@ query {
       <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
       <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
     </p>
+    -->
 
-    <!--
     <div v-for='page in $page.allGoogleSheet.edges' :key='page.id'>
       {{ page.node.name }}
     </div>
-    -->
 
   </Layout>
 </template>
