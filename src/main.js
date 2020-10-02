c1d6b83e66204b73cafe7e0e1afd007cb94ed26e
krasnokutskiy.e <Krasnokutskiy.E@ostec-group.ru>
2020-10-03 01:42:50 +0300

    added custom font

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
+import 'typeface-amatic-sc'
 
 import DefaultLayout from '~/layouts/Default.vue'
 
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -11,7 +11,7 @@ export default {
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt='Example image' src='~/favicon.png' width='135' />
 
-    <h1>Hello, world!</h1>
+    <h1 class='text-red-700 font-body text-4xl'>Hello, world!</h1>
 
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
