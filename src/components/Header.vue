09bca58cea9fe730c9b8319e8de7ffb0479e23c9
krasnokutskiy.e <Krasnokutskiy.E@ostec-group.ru>
2020-10-15 03:37:11 +0300

    WIP

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,4 +1,6 @@
 <script>
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { disableBodyScroll, enableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock'
 import MenuButton from './MenuButton'
 // import debounce from 'lodash.debounce'
@@ -13,14 +15,16 @@ export default {
   data () {
     return {
       isOpenedMenu: true,
-      isMobileView: false
+      isMobileView: false,
+      isInvertedColor: false
     }
   },
 
   watch: {
     isMobileView (newValue, oldValue) {
       !newValue && oldValue
-        ? this.$set(this, 'isOpenedMenu', true) : this.$set(this, 'isOpenedMenu', false)
+        ? this.$set(this, 'isOpenedMenu', true) && enableBodyScroll(this.$refs.menuList)
+        : this.$set(this, 'isOpenedMenu', false)
     },
 
     isOpenedMenu (newValue, oldValue) {
@@ -30,6 +34,7 @@ export default {
   },
 
   created () {
+    gsap.registerPlugin(ScrollTrigger)
     process.browser && window.addEventListener('resize', this.detectMobile)
   },
 
@@ -39,6 +44,7 @@ export default {
 
   mounted () {
     this.detectMobile()
+    this.startAnimation()
   },
 
   methods: {
@@ -49,6 +55,17 @@ export default {
 
     toggleMenuButton () {
       this.isOpenedMenu = !this.isOpenedMenu
+    },
+
+    startAnimation () {
+      ScrollTrigger.create({
+        trigger: this.$refs.headerContent,
+        start: 'top top', // 'element scrubber'
+        end: 'bottom 78px', // 'element scrubber'
+        onEnter: () => this.$set(this, 'isInvertedColor', true),
+        onEnterBack: () => this.$set(this, 'isInvertedColor', false)
+        // markers: true
+      })
     }
   }
 }
@@ -57,8 +74,8 @@ export default {
 <template>
   <header ref='header' class='text-gray-700 body-font'>
     <!-- logo -->
-    <div class='fixed flex flex-wrap px-10 py-5 flex-row items-center bg-green-300 w-screen'>
-      <a class='flex title-font font-medium items-center text-gray-900'>
+    <div ref='headerContent' :class='[isInvertedColor ? "bg-red-600 text-white" : "bg-green-300", "fixed flex flex-wrap px-10 py-5 flex-row items-center w-screen transition-colors duration-500 ease-in-out"]'>
+      <a class='flex title-font font-medium items-center'>
         <svg
           xmlns='http://www.w3.org/2000/svg'
           fill='none'
@@ -66,7 +83,7 @@ export default {
           stroke-linecap='round'
           stroke-linejoin='round'
           stroke-width='2'
-          class='w-10 h-10 text-white p-2 bg-indigo-500 rounded-full'
+          class='w-10 h-10 text-white p-2 bg-indigo-500 rounded-full logomy'
           viewBox='0 0 24 24'
         >
           <path d='M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5' />
@@ -78,18 +95,21 @@ export default {
     <!-- -->
 
     <!-- menu list -->
-    <div v-show='isOpenedMenu' ref='menuList' class='flex fixed w-screen h-screen right-0 bg-green-300 md:mr-10 md:mt-6 md:w-auto md:h-8'>
-      <nav class='flex flex-wrap flex-col text-3xl pl-10 mt-4 md:pl-0 md:mt-0 md:text-base md:w-auto md:h-auto md:flex-row md:ml-auto md:items-center md:justify-center'>
-        <g-link class='mb-2 md:mr-5 md:mb-0 nav__link hover:text-gray-900' to='/'>Home</g-link>
-        <a class='mb-2 md:mr-5 md:mb-0 hover:text-gray-900'>Second Link</a>
-        <a class='mb-2 md:mr-5 md:mb-0 hover:text-gray-900'>Third Link</a>
-        <g-link class='nav__link hover:text-gray-900' to='/about/'>About</g-link>
-      </nav>
-    </div>
+    <transition name='fade'>
+      <div v-show='isOpenedMenu' ref='menuList' :class='[isInvertedColor ? "bg-red-600 text-white" : "bg-green-300", "flex fixed w-screen h-screen right-0 md:mr-10 md:mt-6 md:w-auto md:h-8 transition-colors duration-500 ease-in-out"]'>
+        <nav class='flex flex-wrap flex-col text-3xl pl-10 mt-4 md:pl-0 md:mt-0 md:text-base md:w-auto md:h-auto md:flex-row md:ml-auto md:items-center md:justify-center'>
+          <g-link class='mb-2 md:mr-5 md:mb-0 nav__link hover:text-gray-900' to='/'>Home</g-link>
+          <a class='mb-2 md:mr-5 md:mb-0 hover:text-gray-900'>Second Link</a>
+          <a class='mb-2 md:mr-5 md:mb-0 hover:text-gray-900'>Third Link</a>
+          <g-link class='nav__link hover:text-gray-900' to='/about/'>About</g-link>
+        </nav>
+      </div>
+    </transition>
     <!-- -->
 
     <!-- menu button burger -->
     <MenuButton
+      :is-inverted-color='isInvertedColor'
       :is-opened-menu='isOpenedMenu'
       class='fixed mt-4 mr-8 top-0 right-0 md:hidden'
       @toggle-menu-button='toggleMenuButton'
@@ -97,3 +117,12 @@ export default {
     <!-- -->
   </header>
 </template>
+
+<style>
+.fade-enter-active, .fade-leave-active {
+  transition: transform .5s;
+}
+.fade-enter, .fade-leave-to {
+  transform: translateX(-100%);
+}
+</style>
